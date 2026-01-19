ca4f7f1cfd91c717aeda5efc6fed44f9da99e358
loggicdev <loggic.dev@gmail.com>
2026-01-19 13:44:30 -0300

    subir

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const KLAVIYO_API_KEY = process.env.KLAVIYO_API_KEY;
 const KLAVIYO_LIST_ID = 'V3FZ83';
